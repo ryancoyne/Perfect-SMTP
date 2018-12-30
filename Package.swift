1800c9780e90bc39fac5a11c33179c0e05165ff7
Ryan <ryan@buckettechnologies.com>
2018-12-30 15:55:25 -0500

    Updating the package file

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(name: "PerfectSMTP", 
 	products: [.library(name: "PerfectSMTP",targets: ["PerfectSMTP"]),],
     dependencies: [
-	.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from: "3.0.6"),
+	.package(url: "https://github.com/ryancoyne/Perfect-CURL.git", from: "4.0.0"),
 	],
     targets: [
         .target(
